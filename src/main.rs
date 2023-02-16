1dd32a203e1b362cfe28bb0a72e28462a9ea9028
e5f6bd <41416412+e5f6bd@users.noreply.github.com>
2023-02-17 02:32:30 +0900

    Tried to write frequency, but failed

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use anyhow::Context;
+use std::{collections::BTreeSet, iter::once, time::Duration};
+
+use anyhow::{bail, Context};
 use rusb::{DeviceList, UsbOption};
 
 fn main() -> anyhow::Result<()> {
@@ -18,21 +20,40 @@ fn main() -> anyhow::Result<()> {
         .next()
         .context("Interface descriptor not found")?;
 
-    let handle = device.open()?;
+    let mut handle = device.open()?;
     println!(
         "Kernel driver? => {:?}",
-        handle.kernel_driver_active(interface_desc.interface_number())?
+        handle.kernel_driver_active(interface_desc.interface_number())
     );
 
-    // for interface in conf_desc.interfaces() {
-    //     println!("interface");
-    //     for interface_desc in interface.descriptors() {
-    //         println!("  interface_desc {interface_desc:?}");
-    //         for endpoint_desc in interface_desc.endpoint_descriptors() {
-    //             println!("    endpoint_desc address={:?}", endpoint_desc.address());
-    //         }
-    //     }
-    // }
+    handle.set_active_configuration(conf_desc.number())?;
+    let iface_num = interface_desc.interface_number();
+    handle.claim_interface(iface_num)?;
+    handle.set_alternate_setting(iface_num, interface_desc.setting_number())?;
+
+    let addresses = interface_desc
+        .endpoint_descriptors()
+        .map(|e| e.address())
+        .collect::<BTreeSet<_>>();
+    if !addresses.contains(&0x04) {
+        bail!("Required endpoints were not found!");
+    }
+
+    let timeout = Duration::from_secs(1);
+    // Enable only channel 3 by manipulating register 0x00
+    handle.write_bulk(0x04, &bytes_to_bits([0b_0000_0000, 0b_1000_0000]), timeout)?;
+    // Set the frequency to about 0.6 MHz by manipulating register 0x04
+    handle.write_bulk(
+        0x04,
+        &bytes_to_bits(once(0b_0000_0100).chain(0x004EA4A9_u32.to_be_bytes())),
+        timeout,
+    )?;
 
     Ok(())
+}
+
+fn bytes_to_bits(v: impl IntoIterator<Item = u8>) -> Vec<u8> {
+    v.into_iter()
+        .flat_map(|v| (0..8).map(move |i| if v & 1 << i > 0 { 1 } else { 0 }))
+        .collect()
 }
