dd1b5f4d83e6967888211288a7dbd47764a89423
e5f6bd <41416412+e5f6bd@users.noreply.github.com>
2023-02-17 13:39:34 +0900

    Revert "Sleep every time so that we ma"
    This reverts commit 79026ec9dc89a087353c870749e51782edcf646f.

--- src/main.rs
+++ src/main.rs
@@ -4,46 +4,32 @@ use anyhow::{bail, Context};
 use rusb::{DeviceList, UsbOption};
 
 fn main() -> anyhow::Result<()> {
-    let sleep = || {
-        println!("Now: {}", chrono::offset::Local::now());
-        std::thread::sleep(Duration::from_secs(1));
-    };
-
     let context = rusb::Context::with_options(&[UsbOption::use_usbdk()])?;
-    sleep();
     let (dev_desc, device) = DeviceList::new_with_context(context)?
         .iter()
         .filter_map(|d| Some((d.device_descriptor().ok()?, d)))
         .find(|(desc, device)| desc.vendor_id() == 0x0456 && desc.product_id() == 0xee25)
         .context("AD9959 not found")?;
-    sleep();
     let conf_desc = device.config_descriptor(0)?;
-    sleep();
     let interface = conf_desc
         .interfaces()
         .next()
         .context("Interface not found")?;
-    sleep();
     let interface_desc = interface
         .descriptors()
         .next()
         .context("Interface descriptor not found")?;
-    sleep();
 
     let mut handle = device.open()?;
     println!(
         "Kernel driver? => {:?}",
         handle.kernel_driver_active(interface_desc.interface_number())
     );
-    sleep();
 
     handle.set_active_configuration(conf_desc.number())?;
-    sleep();
     let iface_num = interface_desc.interface_number();
     handle.claim_interface(iface_num)?;
-    sleep();
     handle.set_alternate_setting(iface_num, interface_desc.setting_number())?;
-    sleep();
 
     let addresses = interface_desc
         .endpoint_descriptors()
@@ -52,19 +38,16 @@ fn main() -> anyhow::Result<()> {
     if !addresses.contains(&0x04) {
         bail!("Required endpoints were not found!");
     }
-    sleep();
 
     let timeout = Duration::from_secs(1);
     // Enable only channel 3 by manipulating register 0x00
     handle.write_bulk(0x04, &bytes_to_bits([0b_0000_0000, 0b_1000_0000]), timeout)?;
-    sleep();
     // Set the frequency to about 0.6 MHz by manipulating register 0x04
     handle.write_bulk(
         0x04,
         &bytes_to_bits(once(0b_0000_0100).chain(0x004EA4A9_u32.to_be_bytes())),
         timeout,
     )?;
-    sleep();
 
     Ok(())
 }
